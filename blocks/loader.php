4a35bc42025f2cd7a75c5d54988db41c2484f771
Hoang Huong <70555229+codetot-huong@users.noreply.github.com>
2021-04-29 11:15:15 +0700

    Update loader style
    (cherry picked from commit cd1e4478bce08c79557abd806a150b234d15d16b)

--- blocks/loader.php
+++ blocks/loader.php
@@ -1,15 +1,18 @@
 <div class="loader<?php if (!empty($class) ) : echo ' ' . $class; endif; ?>">
 	<div class="loader__inner">
 		<div class="lds-grid">
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
-			<div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
 		</div>
 	</div>
 </div>
