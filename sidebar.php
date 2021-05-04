a7bfe56296376459f9f9b1be96bb4719d1c6d2f8
Khoi Nguyen <khoi@codetot.com>
2021-05-04 19:11:18 +0700

    Check condition before output sidebar

--- sidebar.php
+++ sidebar.php
@@ -4,14 +4,16 @@
  * @since 5.0.0
  * @author codetot
  */
-do_action('codetot_before_sidebar');
-
 $sidebar = codetot_sidebar_id();
 
-?>
+if (is_active_sidebar($sidebar)) : ?>
 
-<aside id="secondary" class="widget-area">
-	<?php dynamic_sidebar( $sidebar ); ?>
-</aside><!-- #secondary -->
+  <?php do_action('codetot_before_sidebar'); ?>
 
-<?php do_action('codetot_after_sidebar'); ?>
+  <aside id="secondary" class="widget-area">
+    <?php dynamic_sidebar( $sidebar ); ?>
+  </aside><!-- #secondary -->
+
+  <?php do_action('codetot_after_sidebar'); ?>
+
+<?php endif; ?>
