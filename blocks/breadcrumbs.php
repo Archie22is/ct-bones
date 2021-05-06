e9b7bd9904f0665562d83b9431856bf70fbab569
Thaophuong <thao@codetot.com>
2021-05-06 13:56:19 +0700

    Update Breadcrumns

--- blocks/breadcrumbs.php
+++ blocks/breadcrumbs.php
@@ -1,10 +1,12 @@
 <?php
 $container_class = codetot_site_container();
 $_class = !empty($class) ? ' ' . esc_attr($class) : '';
-
 ?>
 <div class="breadcrumbs<?php echo $_class; ?>">
   <div class="<?php echo $container_class; ?> breadcrumbs__container">
+  <?php if($enable_title_page) : ?>
+    <h2 class="breadcrumbs__title"><?php echo get_the_title(); ?></h2>
+  <?php endif; ?>
     <?php codetot_breadcrumbs(); ?>
   </div>
 </div>
