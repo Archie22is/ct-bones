2febed41dde493c8328e415cadce655c51fd7055
Khoi Nguyen <khoi@codetot.com>
2021-05-07 00:13:24 +0700

    Fix countdown state in widget, set condition to only single product price, add new style Tiki countdown

--- src/js/woocommerce-blocks/countdown-price.js
+++ src/js/woocommerce-blocks/countdown-price.js
@@ -1,4 +1,4 @@
-import { select, getData, addClass } from 'lib/dom'
+import { select, getData, addClass, closest } from 'lib/dom'
 
 const getTimeRemaining = endtime => {
   const total = Date.parse(endtime) - Date.parse(new Date())
@@ -16,9 +16,32 @@ const getTimeRemaining = endtime => {
   }
 }
 
+const renderHtml = (price, label) => {
+  return `
+    <span class="single-product__price__top">${price}</span>
+    <span class="single-product__price__bottom">
+      <span class="single-product__price__label">${label}</span>
+      <span class="single-product__price__countdown js-countdown">
+        <span class="single-product__price__item single-product__price__days js-days"></span>
+        <span class="single-product__price__item single-product__price__hours js-hours"></span>
+        <span class="single-product__price__item single-product__price__minutes js-minutes"></span>
+        <span class="single-product__price__item single-product__price__seconds js-seconds"></span>
+      </span>
+      <span class="single-product__price__notice js-notice"></span>
+    </span>
+  `
+}
+
 export default el => {
+  const parentEl = closest('.entry-summary', el)
   const endDate = getData('end-date', el)
   const labels = getData('labels', el) ? JSON.parse(getData('labels', el)) : []
+  let noticeEl = null
+  let daysEl = null
+  let hoursEl = null
+  let minutesEl = null
+  let secondsEl = null
+  let rendered = false
 
   const getLabels = (number, type) => {
     const matchingLabel =
@@ -31,48 +54,67 @@ export default el => {
   const displayMessage = messageType => {
     if (noticeEl && getMessage(messageType)) {
       noticeEl.innerHTML = getMessage(messageType)
+      addClass('display-message', el)
     }
   }
 
-  const noticeEl = select('.js-notice', el)
-  const daysEl = select('.js-days', el)
-  const hoursEl = select('.js-hours', el)
-  const minutesEl = select('.js-minutes', el)
-  const secondsEl = select('.js-seconds', el)
+  const render = () => {
+    const label = labels.message.ongoing
+    const existingPriceHtml = el.innerHTML
 
-  const updateRemainingTime = () => {
-    const { total, days, hours, minutes, seconds } = getTimeRemaining(endDate)
+    el.innerHTML = renderHtml(existingPriceHtml, label)
 
-    if (daysEl && days) {
-      daysEl.innerHTML = getLabels(days, 'days')
+    noticeEl = select('.js-notice', el)
+    daysEl = select('.js-days', el)
+    hoursEl = select('.js-hours', el)
+    minutesEl = select('.js-minutes', el)
+    secondsEl = select('.js-seconds', el)
 
-      if (days < 1) {
-        displayMessage('less_day')
-      }
-    }
+    addClass('is-loaded', el)
 
-    if (hoursEl && hours) {
-      hoursEl.innerHTML = getLabels(hours, 'hours')
-
-      if (hours < 1) {
-        displayMessage('less_hour')
-      }
-    }
-
-    if (minutesEl && minutes) {
-      minutesEl.innerHTML = getLabels(minutes, 'minutes')
-    }
-
-    if (secondsEl && seconds) {
-      secondsEl.innerHTML = getLabels(('0' + seconds).slice(-2), 'seconds')
-    }
-
-    if (total <= 0) {
-      displayMessage('expired')
-      addClass('is-hide', el)
-      clearInterval(timeinterval)
-    }
+    rendered = true
   }
 
-  const timeinterval = setInterval(updateRemainingTime, 1000)
+  if (parentEl) {
+    const updateRemainingTime = () => {
+      if (!rendered) {
+        render()
+      }
+
+      const { total, days, hours, minutes, seconds } = getTimeRemaining(endDate)
+
+      if (daysEl && days) {
+        daysEl.innerHTML = getLabels(days, 'days')
+
+        if (days < 1) {
+          displayMessage('less_day')
+        }
+      }
+
+      if (hoursEl && hours) {
+        hoursEl.innerHTML = getLabels(hours, 'hours')
+
+        if (hours < 1) {
+          displayMessage('less_hour')
+        }
+      }
+
+      if (minutesEl && minutes) {
+        minutesEl.innerHTML = getLabels(minutes, 'minutes')
+      }
+
+      if (secondsEl && seconds) {
+        secondsEl.innerHTML = getLabels(('0' + seconds).slice(-2), 'seconds')
+      }
+
+      if (total <= 0) {
+        displayMessage('expired')
+        addClass('is-hide', el)
+        clearInterval(timeinterval)
+      }
+    }
+
+    // eslint-disable-next-line no-unused-vars
+    const timeinterval = setInterval(updateRemainingTime, 1000)
+  }
 }
