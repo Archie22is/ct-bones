6d28f2f6ac8732f25fcce7c862374d20a50e2264
Khoi Nguyen <khoi@codetot.com>
2021-05-08 18:54:19 +0700

    Set condition to display modal login

--- block-parts/header/account-icon.php
+++ block-parts/header/account-icon.php
@@ -1,18 +1,14 @@
 <?php
+  ob_start(); ?>
+  <a class="header__menu-icons__item header__menu-icons__link header__menu-icons__item--account" href="<?php echo wc_get_account_endpoint_url('dashboard'); ?>">
+     <span class="header__menu-icons__icon">
+       <?php codetot_svg('user', true); ?>
+     </span>
+     <span class="screen-reader-text"><?php _e('Click to manage an account', 'ct-bones'); ?></span>
+   </a>
+ <?php $html = ob_get_clean();
+
 $hide_icon = get_global_option('codetot_header_hide_account_icon') ?? false;
 if (!$hide_icon && class_exists('WooCommerce')) :
-  if ( is_user_logged_in() ) : ?>
-   <a class="header__menu-icons__item header__menu-icons__link header__menu-icons__item--account" href="<?php echo wc_get_account_endpoint_url('dashboard'); ?>">
-      <span class="header__menu-icons__icon">
-        <?php codetot_svg('user', true); ?>
-      </span>
-      <span class="screen-reader-text"><?php _e('Click to manage an account', 'ct-bones'); ?></span>
-      </a>
-  <?php  else : ?>
-    <button class="header__menu-icons__item header__menu-icons__link header__menu-icons__item--account" data-open-modal="modal-login">
-      <span class="header__menu-icons__icon">
-        <?php codetot_svg('user', true); ?>
-      </span>
-    </button>
-  <?php endif; ?>
-<?php endif; ?>
+  echo apply_filters('codetot_header_account_icon', $html);
+endif; ?>
