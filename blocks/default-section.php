d4c2bf25b42f9a7ed00864ea96ebed68bb695d23
Khoi Nguyen <khoi@codetot.com>
2021-05-12 08:50:19 +0700

    Add hooks to default section for extending

--- blocks/default-section.php
+++ blocks/default-section.php
@@ -6,17 +6,19 @@ $container = codetot_site_container();
 $_class = 'default-section';
 $_class .= !empty($class) ? ' ' . $class : '';
 
-$_content = '';
 if (!empty($content)) {
   if (!is_array($content)) {
     $_content = $content;
   } else {
     $_content = codetot_build_grid_columns($content, 'default-section');
   }
+} else {
+  $_content = '';
 }
 
 if (!empty($content)) : ?>
   <section class="<?php echo $_class; ?>"<?php if (!empty($_attrs)) : echo ' ' . $_attrs; endif; ?>>
+    <?php if (!empty($before_header)) : echo $before_header; endif; ?>
     <?php if (!empty($header)) : ?>
       <div class="default-section__header">
         <div class="<?php echo $container; ?> default-section__container default-section__container--header">
@@ -26,6 +28,7 @@ if (!empty($content)) : ?>
         </div>
       </div>
     <?php endif; ?>
+    <?php if (!empty($before_main)) : echo $before_main; endif; ?>
     <div class="default-section__main">
       <div class="<?php echo $container; ?> default-section__container default-section__container--main">
         <div class="default-section__inner default-section__inner--main">
@@ -33,6 +36,7 @@ if (!empty($content)) : ?>
         </div>
       </div>
     </div>
+    <?php if (!empty($after_main)) : echo $after_main; endif; ?>
     <?php if (!empty($footer)) : ?>
       <div class="default-section__footer">
         <div class="<?php echo $container; ?> default-section__container default-section__container--footer">
@@ -42,5 +46,6 @@ if (!empty($content)) : ?>
         </div>
       </div>
     <?php endif; ?>
+    <?php if (!empty($after_footer)) : echo $after_footer; endif; ?>
   </section>
 <?php endif; ?>
