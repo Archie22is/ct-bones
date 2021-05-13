922bf0237729ea991f920046908f2bc7c2cf90ec
Khoi Nguyen <khoi@codetot.com>
2021-05-13 22:18:00 +0700

    Fix a11y audit
    (cherry picked from commit a05645011f3b7de1593c36c43ec426b0f3b97b32)

--- blocks/social-links.php
+++ blocks/social-links.php
@@ -17,7 +17,14 @@ if (empty($items)) {
       <span class="label-text social-links__label"><?php echo $label; ?></span>
     <?php endif; ?>
     <?php foreach ($items as $item) : ?>
-      <a class="social-links__item" data-type="<?php echo $item['type']; ?>" href="<?php echo !empty($item['url']) ? $item['url'] : '#' ?>" target="_blank" rel="noreferrer">
+      <a
+      class="social-links__item"
+      data-type="<?php echo $item['type']; ?>"
+      href="<?php echo !empty($item['url']) ? $item['url'] : '#' ?>"
+      target="_blank"
+      rel="noreferrer"
+      title="<?php printf(__('Visit %s', 'ct-bones'), $item['type']); ?>"
+      >
         <span class="social-links__svg"><?php codetot_svg('social-' . $item['type'], true); ?></span>
         <span class="small-text d-none social-links__text"><?php printf(__('Visit %s', 'ct-bones'), $item['type']); ?></span>
       </a>
