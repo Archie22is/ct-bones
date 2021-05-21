bd929951865a3fe7c21657f9a1158b24e2b97276
Khoi Nguyen <khoi@codetot.com>
2021-05-21 15:11:36 +0700

    Format file php

--- template-parts/content.php
+++ template-parts/content.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying posts
  *
@@ -10,25 +11,28 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
-		if ( is_singular() ) :
-			the_title( '<h1 class="entry-title">', '</h1>' );
-		else :
-			the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-		endif;
+  <header class="entry-header">
+    <?php
+    if (is_singular()) :
+      the_title('<h1 class="entry-title">', '</h1>');
+    else :
+      the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
+    endif;
 
-		if ( 'post' === get_post_type() ) :
-			?>
-			<div class="entry-meta">
-				<?php
-				ct_bones_posted_on();
-				ct_bones_posted_by();
-        ct_bones_entry_categories();
-				?>
-			</div><!-- .entry-meta -->
-		<?php endif; ?>
-	</header><!-- .entry-header -->
+    if ('post' === get_post_type()) :
+      $hide_post_meta = get_global_option('codetot_post_hide_meta') ?? false;
+      if (!$hide_post_meta) :
+    ?>
+        <div class="entry-meta">
+          <?php
+          ct_bones_posted_on();
+          ct_bones_posted_by();
+          ct_bones_entry_categories();
+          ?>
+        </div><!-- .entry-meta -->
+      <?php endif; ?>
+    <?php endif; ?>
+  </header><!-- .entry-header -->
 
   <?php if (apply_filters('codetot_display_post_thumbnail', true) === true) : ?>
     <div class="entry-thumbnail">
@@ -36,37 +40,37 @@
     </div>
   <?php endif; ?>
 
-	<div class="entry-content">
-		<?php
-		the_content(
-			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'ct-bones' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				wp_kses_post( get_the_title() )
-			)
-		);
+  <div class="entry-content">
+    <?php
+    the_content(
+      sprintf(
+        wp_kses(
+          /* translators: %s: Name of current post. Only visible to screen readers */
+          __('Continue reading<span class="screen-reader-text"> "%s"</span>', 'ct-bones'),
+          array(
+            'span' => array(
+              'class' => array(),
+            ),
+          )
+        ),
+        wp_kses_post(get_the_title())
+      )
+    );
 
-		wp_link_pages(
-			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'ct-bones' ),
-				'after'  => '</div>',
-			)
-		);
-		?>
-	</div><!-- .entry-content -->
+    wp_link_pages(
+      array(
+        'before' => '<div class="page-links">' . esc_html__('Pages:', 'ct-bones'),
+        'after'  => '</div>',
+      )
+    );
+    ?>
+  </div><!-- .entry-content -->
 
-	<footer class="entry-footer">
-		<?php
+  <footer class="entry-footer">
+    <?php
     ct_bones_entry_tags();
     ct_bones_entry_comment_links();
     ct_bones_entry_footer();
     ?>
-	</footer><!-- .entry-footer -->
+  </footer><!-- .entry-footer -->
 </article><!-- #post-<?php the_ID(); ?> -->
