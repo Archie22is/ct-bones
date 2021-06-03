ab832e944345232915b0fe3c446370452d82a7ef
Khoi Nguyen <khoi@codetot.com>
2021-06-03 16:29:16 +0700

    Remove generate main wrapper in woocommerce

--- inc/woocommerce.php
+++ inc/woocommerce.php
@@ -1,46 +1,5 @@
 <?php
-/**
- * WooCommerce Compatibility File
- *
- * @link https://woocommerce.com/
- *
- * @package CT_Bones
- */
-/**
- * Remove default WooCommerce wrapper.
- */
+
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10 );
 remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10 );
-
-if ( ! function_exists( 'ct_bones_woocommerce_wrapper_before' ) ) {
-	/**
-	 * Before Content.
-	 *
-	 * Wraps all WooCommerce content in wrappers which match the theme markup.
-	 *
-	 * @return void
-	 */
-	function ct_bones_woocommerce_wrapper_before() {
-		?>
-			<main id="primary" class="site-main">
-		<?php
-	}
-}
-add_action( 'woocommerce_before_main_content', 'ct_bones_woocommerce_wrapper_before' );
-
-if ( ! function_exists( 'ct_bones_woocommerce_wrapper_after' ) ) {
-	/**
-	 * After Content.
-	 *
-	 * Closes the wrapping divs.
-	 *
-	 * @return void
-	 */
-	function ct_bones_woocommerce_wrapper_after() {
-		?>
-			</main><!-- #main -->
-		<?php
-	}
-}
-add_action( 'woocommerce_after_main_content', 'ct_bones_woocommerce_wrapper_after' );
 
