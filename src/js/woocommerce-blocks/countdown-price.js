861627945210931dbb2b85d349adcd768e857cde
Khoi Nguyen <khoi@codetot.com>
2021-06-17 10:49:43 +0700

    Add countdown support for variant product, add upcoming visibility

--- src/js/woocommerce-blocks/countdown-price.js
+++ src/js/woocommerce-blocks/countdown-price.js
@@ -1,7 +1,9 @@
-import { select, getData, addClass, closest } from 'lib/dom'
+/* global CODETOT_COUNTDOWN_LABELS */
+import { select, getData, addClass, removeClass, closest } from 'lib/dom'
 
-const getTimeRemaining = endtime => {
-  const total = Date.parse(endtime) - Date.parse(new Date())
+const getTimeRemaining = (time, type = 'end') => {
+  const total = type === 'end' ? time - Date.parse(new Date()) : Date.parse(new Date()) - time
+
   const seconds = Math.floor((total / 1000) % 60)
   const minutes = Math.floor((total / 1000 / 60) % 60)
   const hours = Math.floor((total / (1000 * 60 * 60)) % 24)
@@ -16,59 +18,64 @@ const getTimeRemaining = endtime => {
   }
 }
 
-const renderHtml = (price, label) => {
+const renderHtml = price => {
   return `
     <span class="single-product__price__top">${price}</span>
     <span class="single-product__price__bottom">
-      <span class="single-product__price__label">${label}</span>
+      <span class="single-product__price__label js-label"></span>
       <span class="single-product__price__countdown js-countdown">
         <span class="single-product__price__item single-product__price__days js-days"></span>
         <span class="single-product__price__item single-product__price__hours js-hours"></span>
         <span class="single-product__price__item single-product__price__minutes js-minutes"></span>
         <span class="single-product__price__item single-product__price__seconds js-seconds"></span>
       </span>
-      <span class="single-product__price__notice js-notice"></span>
     </span>
+    <span class="single-product__price__notice js-notice"></span>
   `
 }
 
 export default el => {
   const parentEl = closest('.entry-summary', el)
-  const endDate = getData('end-date', el)
-  const labels = getData('labels', el) ? JSON.parse(getData('labels', el)) : []
+  const startDate = Date.parse(getData('start-date', el))
+  const endDate = Date.parse(getData('end-date', el))
+  const labels = CODETOT_COUNTDOWN_LABELS ? JSON.parse(CODETOT_COUNTDOWN_LABELS) : []
   let noticeEl = null
   let daysEl = null
   let hoursEl = null
   let minutesEl = null
   let secondsEl = null
+  let labelEl = null
   let rendered = false
 
   const getLabels = (number, type) => {
     const matchingLabel =
-      parseInt(number) > 1 ? labels[type].plural : labels[type].singular
+      parseInt(number) > 0 ? labels[type].plural : labels[type].singular
 
     return `<span class="single-product__price__bottom__inner"><span class="number">${number}</span> <span class="unit">${matchingLabel}</span></span>`
   }
 
   const getMessage = messageType => labels['message'][messageType]
   const displayMessage = messageType => {
+    removeClass('display-message', el)
+
     if (noticeEl && getMessage(messageType)) {
       noticeEl.innerHTML = getMessage(messageType)
       addClass('display-message', el)
     }
+
+    return el
   }
 
   const render = () => {
-    const label = labels.message.ongoing
     const existingPriceHtml = el.innerHTML
-
-    el.innerHTML = renderHtml(existingPriceHtml, label)
+    el.innerHTML = renderHtml(existingPriceHtml)
 
     noticeEl = select('.js-notice', el)
     daysEl = select('.js-days', el)
     hoursEl = select('.js-hours', el)
     minutesEl = select('.js-minutes', el)
     secondsEl = select('.js-seconds', el)
+    labelEl = select('.js-label', el)
 
     addClass('is-loaded', el)
 
@@ -76,46 +83,106 @@ export default el => {
   }
 
   if (parentEl) {
+    const updateDay = days => {
+      if (daysEl) {
+        daysEl.innerHTML = getLabels(days, 'days')
+
+        if (days < 1) {
+          displayMessage('less_day')
+          addClass('hide-day', el)
+        }
+      }
+    }
+
+    const updateHour = hours => {
+      if (hoursEl) {
+        hoursEl.innerHTML = getLabels(hours, 'hours')
+
+        if (hours < 1) {
+          displayMessage('less_hour')
+          addClass('hide-hour', el)
+        }
+      }
+    }
+
+    const updateMinute = minutes => {
+      if (minutesEl && minutes) {
+        minutesEl.innerHTML = getLabels(minutes, 'minutes')
+      }
+    }
+
+    const updateSecond = seconds => {
+      if (secondsEl && seconds) {
+        secondsEl.innerHTML = getLabels(('0' + seconds).slice(-2), 'seconds')
+      }
+    }
+
+    const updateOnGoingTime = () => {
+      const { days, hours, minutes, seconds } = getTimeRemaining(endDate, 'end')
+
+      updateDay(days)
+      updateHour(hours)
+      updateMinute(minutes)
+      updateSecond(seconds)
+    }
+
+    const updateScheduledTime = () => {
+      const { days, hours, minutes, seconds } = getTimeRemaining(startDate, 'start')
+
+      updateDay(days)
+      updateHour(hours)
+      updateMinute(minutes)
+      updateSecond(seconds)
+    }
+
     const updateRemainingTime = () => {
       if (!rendered) {
         render()
       }
 
-      const { total, days, hours, minutes, seconds } = getTimeRemaining(endDate)
+      const currentTime = Date.parse(new Date())
 
-      if (daysEl && days) {
-        daysEl.innerHTML = getLabels(days, 'days')
+      if (
+        startDate && startDate > currentTime
+      ) {
+        updateScheduledTime()
+        labelEl.innerHTML = getMessage('not_start')
 
-        if (days < 1) {
-          displayMessage('less_day')
+        const { total } = getTimeRemaining(startDate)
+
+        if (total <= 0) {
+          reset()
         }
-      }
+      } else if (
+        endDate && endDate >= currentTime
+      ) {
+        labelEl.innerHTML = getMessage('ongoing')
 
-      if (hoursEl && hours) {
-        hoursEl.innerHTML = getLabels(hours, 'hours')
+        updateOnGoingTime()
 
-        if (hours < 1) {
-          displayMessage('less_hour')
+        const { total } = getTimeRemaining(endDate)
+
+        if (total <= 0) {
+          reset()
         }
-      }
+      } else {
+        reset()
 
-      if (minutesEl && minutes) {
-        minutesEl.innerHTML = getLabels(minutes, 'minutes')
-      }
-
-      if (secondsEl && seconds) {
-        secondsEl.innerHTML = getLabels(('0' + seconds).slice(-2), 'seconds')
-      }
-
-      if (total <= 0) {
-        displayMessage('expired')
-        addClass('is-hide', el)
-        clearInterval(timeinterval)
-        window.location.reload()
+        console.log('DEBUG: There is unknown error with countdown settings.')
       }
     }
 
     // eslint-disable-next-line no-unused-vars
     const timeinterval = setInterval(updateRemainingTime, 1000)
+
+    const reset = () => {
+      clearInterval(timeinterval)
+
+      setTimeout(() => {
+        window.location.reload()
+      }, 1000)
+
+      return el
+    }
   }
 }
