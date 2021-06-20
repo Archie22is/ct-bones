13e6fa2ed1bade7aa77c797f1e69585b5a6c803d
Khoi Nguyen <khoi@codetot.com>
2021-06-20 23:45:19 +0700

    Fix form dark using setting class 'gform_dark', add inline via class 'gform_inline'

--- src/js/global.js
+++ src/js/global.js
@@ -1,3 +1,6 @@
+import { select, selectAll } from 'lib/dom'
+import { initStyle } from 'lib/scripts'
+
 const blocks = document.querySelectorAll('[data-block]')
 
 const initBlocks = () => {
@@ -13,4 +16,18 @@ const initBlocks = () => {
   }
 }
 
-document.addEventListener('DOMContentLoaded', initBlocks)
+const checkjQueryUIStyle = () => {
+  const datePickerEl = select('.ui-date-picker')
+  const datePickerTrigger = selectAll('.datepicker')
+
+  if (datePickerEl && datePickerTrigger) {
+    setTimeout(() => {
+      initStyle('https://code.jquery.com/ui/1.12.0/themes/smoothness/jquery-ui.css')
+    }, 10000)
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  initBlocks()
+  checkjQueryUIStyle()
+})
