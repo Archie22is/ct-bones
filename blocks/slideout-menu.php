90695d0173423d1ab45df4f40161ee58cca582f6
Khoi Nguyen <khoi@codetot.com>
2021-06-20 20:04:45 +0700

    Update slideout menu hooks

--- blocks/slideout-menu.php
+++ blocks/slideout-menu.php
@@ -7,19 +7,32 @@
   <div class="slideout-menu__wrapper">
     <div class="slideout-menu__inner">
       <?php
-      echo get_search_form(array('id' => 'slideout-menu'));
+      echo apply_filters('codetot_slideout_menu_search', get_search_form(array('id' => 'slideout-menu')));
       if ( function_exists('icl_object_id') ) {
-        echo do_shortcode( '[wpml_language_switcher type="footer" flags=1 native=0 translated=0][/wpml_language_switcher]' );
+        echo apply_filters('codetot_slideout_menu_wpml_flags', do_shortcode( '[wpml_language_switcher type="footer" flags=1 native=0 translated=0][/wpml_language_switcher]' ));
       }
       ?>
-      <?php if (has_nav_menu('primary')) :
+      <?php
+      $default_menu_location = !empty($menu) ? $menu : 'primary';
+      $menu_location = apply_filters('codetot_slideout_menu_location', $default_menu_location);
+
+      ob_start();
+      if (has_nav_menu($menu_location)) :
         wp_nav_menu(array(
-          'theme_location' => 'primary',
+          'theme_location' => $menu_location,
           'container' => 'nav',
           'container_class' => 'slideout-menu__nav',
           'menu_class' => 'slideout-menu__menu'
         ));
-      endif; ?>
+      else :
+        if (is_user_logged_in()) :
+          printf('<p>%s</p>', __('You need to configure menu.', 'ct-bones'));
+        endif;
+      endif;
+      $menu_html = ob_get_clean();
+
+      echo apply_filters('codetot_slideout_menu_html', $menu_html);
+      ?>
     </div>
   </div>
 </div>
