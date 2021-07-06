1a4198034077d08ef2ded3dd9c1ec652f6ba29a9
Khoi Nguyen <khoi@codetot.com>
2021-07-06 11:04:41 +0700

    Refactor block widget company info, allow to choose dark/light theme

--- codetot/widgets/ct-company-info.php
+++ codetot/widgets/ct-company-info.php
@@ -1,108 +1,169 @@
 <?php
 
-class codetot_widget_company_info extends WP_Widget {
-  function __construct() {
+class codetot_widget_company_info extends WP_Widget
+{
+  function __construct()
+  {
     parent::__construct(
       'codetot_widget_company_info',
       '[CT] Company Info',
-      array( 'description'  =>  'Your site’s most icon box' )
+      array(
+        'classname' => 'widget_codetot_widget_company_info'
+      )
     );
   }
-  function form( $instance ) {
-    $default = array(
-    );
-    $instance = wp_parse_args( (array) $instance, $default );
+  function form($instance)
+  {
+    $default = array();
+    $instance = wp_parse_args((array) $instance, $default);
   }
-  function update( $new_instance, $old_instance ) {
+  function update($new_instance, $old_instance)
+  {
     $instance = $old_instance;
+
     return $instance;
   }
-  function widget( $args, $instance ) {
-    extract($args);
-    $title = get_field('title','widget_' . $args["widget_id"]);
-    $address = get_field('address','widget_' . $args["widget_id"]);
-    $phone = get_field('phone','widget_' . $args["widget_id"]);
-    $email = get_field('email','widget_' . $args["widget_id"]);
-    $zalo = get_field('zalo','widget_' . $args["widget_id"]);
-    $messeger = get_field('messeger','widget_' . $args["widget_id"]);
-    $website = get_field('website','widget_' . $args["widget_id"]);
-    $about = get_field('about','widget_' . $args["widget_id"]);
+  function widget($args, $instance)
+  {
+    // var_dump($args);
 
-    ?>
-      <div id="contact-info" class="widget widget-contact-info">
-        <?php if(!empty($title)) : ?>
-          <p class="widget__title"><?php echo $title; ?></p>
-        <?php endif; ?>
-          <div class="textwidget">
-          <?php if(!empty($about)) : ?>
-              <p><?php echo $about; ?></p>
-          <?php endif; ?>
-          <ul class="footer-contact">
-            <?php if(!empty($phone)) : ?>
-              <li class="footer-contact__item">
-                <a href="tel:<?php echo $phone; ?>">
-                <span class="footer-contact__icon"><?php codetot_svg('hotline', true);?></span>
-                  <span class="footer-contact__content"><?php echo $phone; ?></span>
-                </a>
-              </li>
-            <?php endif; ?>
-            <?php if(!empty($address)) : ?>
-              <li class="footer-contact__item">
-                <span class="footer-contact__icon"><?php codetot_svg('address', true);?></span>
-                <span class="footer-contact__content"><?php echo $address; ?></span>
-              </li>
-            <?php endif; ?>
-            <?php if(!empty($email)) : ?>
-              <li class="footer-contact__item">
-                <a href="mailto:<?php echo $email; ?>">
-                <span class="footer-contact__icon"><?php codetot_svg('email', true);?></span>
-                  <span class="footer-contact__content"><?php echo $email; ?></span>
-                </a>
-              </li>
-            <?php endif; ?>
-            <?php if(!empty($zalo)) : ?>
-              <li class="footer-contact__item">
-                <a href="tel:<?php echo $zalo; ?>">
-                <span class="footer-contact__icon"><?php codetot_svg('social-zalo', true);?></span>
-                  <span class="footer-contact__content"><?php echo $zalo; ?></span>
-                </a>
-              </li>
-            <?php endif; ?>
-            <?php if(!empty($messeger)) : ?>
-              <li class="footer-contact__item">
-                <a href="<?php echo $messeger; ?>">
-                <span class="footer-contact__icon"><?php codetot_svg('social-messenger', true);?></span>
-                  <span class="footer-contact__content"><?php echo $messeger; ?></span>
-                </a>
-              </li>
-            <?php endif; ?>
-            <?php if(!empty($phone)) : ?>
-              <li class="footer-contact__item">
-                <a href="<?php echo $website; ?>">
-                <span class="footer-contact__icon"><?php codetot_svg('global', true);?></span>
-                  <span class="footer-contact__content"><?php echo $website; ?></span>
-                </a>
-              </li>
-            <?php endif; ?>
-          </ul>
-          </div>
-      </div>
-    <?php
+    extract($args);
+
+    $address = get_field('address', 'widget_' . $widget_id);
+    $phone = get_field('phone', 'widget_' . $widget_id);
+    $email = get_field('email', 'widget_' . $widget_id);
+    $zalo = get_field('zalo', 'widget_' . $widget_id);
+    $messeger = get_field('messeger', 'widget_' . $widget_id);
+
+    $theme = get_field('theme', 'widget_' . $widget_id) ?? 'default';
+    $title = get_field('title', 'widget_' . $widget_id);
+    $about = get_field('about', 'widget_' . $widget_id);
+
+    $links = [];
+    if (!empty($phone)) {
+      $links[] = array(
+        'type' => 'phone',
+        'icon' => 'hotline',
+        'title' => sprintf('Call %s', $phone),
+        'url' => sprintf('tel:%s', trim($phone)),
+        'content' => esc_html($phone)
+      );
+    }
+
+    if (!empty($address)) {
+      $links[] = array(
+        'type' => 'address',
+        'icon' => 'address',
+        'content' => $address
+      );
+    }
+
+    if (!empty($email)) {
+      $links[] = array(
+        'type' => 'email',
+        'icon' => 'email',
+        'url' => sprintf('mailto:%s', $email),
+        'title' => sprintf('Send email to %s', $email),
+        'content' => $email
+      );
+    }
+
+    if (!empty($zalo)) {
+      $links[] = array(
+        'type' => 'zalo',
+        'icon' => 'social-zalo',
+        'url' => sprintf('https://zalo.me/%s', $zalo),
+        'title' => sprintf('Send messenger via Zalo %s', $zalo),
+        'content' => $zalo
+      );
+    }
+
+    if (!empty($messenger)) {
+      $links[] = array(
+        'type' => 'messenger',
+        'icon' => 'social-messenger',
+        'url' => sprintf('https://m.me/%s', $messenger),
+        'title' => sprintf('Chat via Messenger %s', $messeger),
+        'content' => $messeger
+      );
+    }
+
+    if (!empty($theme) && $theme !== 'default') {
+      $before_widget = str_replace('class="', 'class="widget--dark-theme ', $before_widget);
+    }
+
+?>
+    <?php echo $before_widget; ?>
+      <?php if (!empty($title)) : ?>
+        <?php echo $before_title; ?><?php echo $title; ?><?php echo $after_title; ?>
+      <?php endif; ?>
+      <?php if (!empty($about)) :
+        printf('<div class="textwidget mt-1 mb-1 wysiwyg widget__intro">%s</div>', $about);
+      endif; ?>
+      <ul class="widget__list">
+        <?php foreach ($links as $link) :
+          ob_start(); ?>
+          <span class="widget__icon"><?php codetot_svg($link['icon'], true); ?></span>
+          <span class="widget__content"><?php echo $link['content']; ?></span>
+          <?php
+          $content = ob_get_clean(); ?>
+
+          <li class="w100 widget__item widget__item--<?php echo esc_attr($link['type']); ?>">
+            <?php if (!empty($link['url'])) :
+              printf(
+                '<a class="f w100 widget__link" href="%1$s" title="%2$s">%3$s</a>',
+                $link['url'],
+                $link['title'],
+                $content
+              );
+            else :
+              echo $content;
+            endif; ?>
+          </li>
+        <?php endforeach; ?>
+      </ul>
+    <?php echo $after_widget; ?>
+<?php
   }
 }
 
-add_action( 'widgets_init', 'create_codetot_widget_company_info' );
-function create_codetot_widget_company_info() {
+add_action('widgets_init', 'create_codetot_widget_company_info');
+function create_codetot_widget_company_info()
+{
   register_widget('codetot_widget_company_info');
 }
 
 // Field
-if( function_exists('acf_add_local_field_group') ):
+if (function_exists('acf_add_local_field_group')) :
 
   acf_add_local_field_group(array(
     'key' => 'group_606fcbaf7210d',
-    'title' => 'Contac info',
+    'title' => 'Widget: Contact info',
     'fields' => array(
+      array(
+        'key' => 'field_60e3d02e95fd1',
+        'label' => 'Theme',
+        'name' => 'theme',
+        'type' => 'radio',
+        'instructions' => '',
+        'required' => 0,
+        'conditional_logic' => 0,
+        'wrapper' => array(
+          'width' => '',
+          'class' => '',
+          'id' => '',
+        ),
+        'choices' => array(
+          'default' => 'Dark Text - Light Background',
+          'dark' => 'Light Text - Dark Background',
+        ),
+        'allow_null' => 0,
+        'other_choice' => 0,
+        'default_value' => 'default',
+        'layout' => 'horizontal',
+        'return_format' => 'value',
+        'save_other_choice' => 0,
+      ),
       array(
         'key' => 'field_606fcbc2b6105',
         'label' => 'Title',
@@ -124,9 +185,9 @@ if( function_exists('acf_add_local_field_group') ):
       ),
       array(
         'key' => 'field_606fd8ce269f3',
-        'label' => 'About',
+        'label' => 'Description',
         'name' => 'about',
-        'type' => 'textarea',
+        'type' => 'wysiwyg',
         'instructions' => '',
         'required' => 0,
         'conditional_logic' => 0,
@@ -136,14 +197,14 @@ if( function_exists('acf_add_local_field_group') ):
           'id' => '',
         ),
         'default_value' => '',
-        'placeholder' => '',
-        'maxlength' => '',
-        'rows' => '',
-        'new_lines' => '',
+        'tabs' => 'all',
+        'toolbar' => 'full',
+        'media_upload' => 0,
+        'delay' => 0
       ),
       array(
         'key' => 'field_606fd2f2d07e1',
-        'label' => 'Địa chỉ',
+        'label' => 'Address',
         'name' => 'address',
         'type' => 'text',
         'instructions' => '',
@@ -156,13 +217,13 @@ if( function_exists('acf_add_local_field_group') ):
         ),
         'default_value' => '',
         'placeholder' => 'Ex: 123 Pham Hung, Bac Tu Liem, Ha Noi',
-        'prepend' => '',
+        'prepend' => 'Address:',
         'append' => '',
         'maxlength' => '',
       ),
       array(
         'key' => 'field_606fd30cd07e2',
-        'label' => 'Điện thoại',
+        'label' => 'Phone',
         'name' => 'phone',
         'type' => 'text',
         'instructions' => '',
@@ -174,8 +235,8 @@ if( function_exists('acf_add_local_field_group') ):
           'id' => '',
         ),
         'default_value' => '',
-        'placeholder' => 'Ex: 01234456789',
-        'prepend' => '',
+        'placeholder' => '0910000000',
+        'prepend' => 'Phone:',
         'append' => '',
         'maxlength' => '',
       ),
@@ -193,8 +254,8 @@ if( function_exists('acf_add_local_field_group') ):
           'id' => '',
         ),
         'default_value' => '',
-        'placeholder' => 'Ex: info@company.com',
-        'prepend' => '',
+        'placeholder' => 'sales@codetot.com',
+        'prepend' => 'Email:',
         'append' => '',
       ),
       array(
@@ -211,8 +272,8 @@ if( function_exists('acf_add_local_field_group') ):
           'id' => '',
         ),
         'default_value' => '',
-        'placeholder' => 'https://zalo.me/0123456789',
-        'prepend' => '',
+        'placeholder' => '0910000000',
+        'prepend' => 'https://zalo.me/',
         'append' => '',
         'maxlength' => '',
       ),
@@ -230,26 +291,10 @@ if( function_exists('acf_add_local_field_group') ):
           'id' => '',
         ),
         'default_value' => '',
-        'placeholder' => 'Ex: https://m.me/codetot',
-        'prepend' => '',
+        'placeholder' => 'codetot',
+        'prepend' => 'https://m.me/',
         'append' => '',
         'maxlength' => '',
-      ),
-      array(
-        'key' => 'field_606fd451af920',
-        'label' => 'Website',
-        'name' => 'website',
-        'type' => 'url',
-        'instructions' => '',
-        'required' => 0,
-        'conditional_logic' => 0,
-        'wrapper' => array(
-          'width' => '',
-          'class' => '',
-          'id' => '',
-        ),
-        'default_value' => '',
-        'placeholder' => 'https://codetot.com',
       ),
     ),
     'location' => array(
@@ -271,5 +316,4 @@ if( function_exists('acf_add_local_field_group') ):
     'description' => '',
   ));
 
-  endif;
-
+endif;
