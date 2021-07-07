abe18ab48f31bd5aac46f1a7ff67d23c19ce7d20
Khoi Nguyen <khoi@codetot.com>
2021-07-07 15:35:03 +0700

    Fix php warning header 6 vertical menu

--- block-parts/header/vertical.php
+++ block-parts/header/vertical.php
@@ -1,25 +1,22 @@
 <?php
+$menu_location = 'vertical_menu';
 $locations = get_nav_menu_locations();
-$menu = wp_get_nav_menu_object($locations['vertical_menu']);
+$menu = !empty($locations[$menu_location]) ? wp_get_nav_menu_object($locations[$menu_location]) : null;
   if (has_nav_menu('vertical_menu')) : ?>
   <div class="grid__col header__col header__col--vertical">
   <div class="header__vertical-wrapper">
     <div class="header__vertical-header">
       <span class="header__vertical-icon"><?php codetot_svg('menu', true); ?></span>
-      <span class="header__vertical-title"><?php echo $menu->name; ?></span>
+      <span class="header__vertical-title"><?php echo !empty($menu) && is_object($menu) ? $menu->name : ''; ?></span>
     </div>
     <div class="header__vertical-list">
-    <?php if (has_nav_menu('primary')) :
-      ob_start();
-      wp_nav_menu(array(
-        'theme_location' => 'vertical_menu',
-        'container' => false,
-        'menu_class' => 'header__vertical_menu'
-      ));
-      $primary_nav_html = ob_get_clean();
-
-      echo $primary_nav_html;
-    endif; ?>
+      <?php if (has_nav_menu($menu_location)) :
+        wp_nav_menu(array(
+          'theme_location' => $menu_location,
+          'container' => false,
+          'menu_class' => 'header__vertical_menu'
+        ));
+      endif; ?>
     </div>
     </div>
   </div>
