3810c5f6cec703fc1b7f6cc036e1c4f335645535
Khoi Nguyen <khoi@codetot.com>
2021-07-07 16:00:15 +0700

    Load asets in js with templatePath variables

--- codetot/assets.php
+++ codetot/assets.php
@@ -150,7 +150,8 @@ class Codetot_Assets
         'url' => admin_url('admin-ajax.php'),
         "ajax_error" => __('Sorry, something went wrong. Please refresh this page and try again!', 'ct-bones'),
         'nonce' => wp_create_nonce('codetot-config-nonce'),
-      )
+      ),
+      'themePath' => get_template_directory_uri()
     );
 
     wp_enqueue_script(
