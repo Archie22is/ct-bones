891597ab2baedf2b37ca772a5e7105767b26560c
Khoi Nguyen <khoi@codetot.com>
2021-07-08 16:39:24 +0700

    Remove archive title prefix

--- codetot/seo-support.php
+++ codetot/seo-support.php
@@ -42,3 +42,20 @@ function codetot_wc_add_gtin8($markup, $product)
   return $markup;
 }
 add_filter('woocommerce_structured_data_product', 'codetot_wc_add_gtin8', 10, 2);
+
+function codetot_archive_title($title)
+{
+  if (is_category()) {
+    $title = single_cat_title('', false);
+  } elseif (is_tag()) {
+    $title = single_tag_title('', false);
+  } elseif (is_author()) {
+    $title = '<span class="vcard">' . get_the_author() . '</span>';
+  } elseif (is_tax()) { //for custom post types
+    $title = sprintf(__('%1$s'), single_term_title('', false));
+  } elseif (is_post_type_archive()) {
+    $title = post_type_archive_title('', false);
+  }
+  return $title;
+}
+add_filter('get_the_archive_title', 'codetot_archive_title');
