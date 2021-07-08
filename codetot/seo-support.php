c611a17013cdd97b8e7ab2850efadcb555b601a7
Khoi Nguyen <khoi@codetot.com>
2021-07-08 15:04:02 +0700

    Fix product schema gtin8

--- codetot/seo-support.php
+++ codetot/seo-support.php
@@ -28,3 +28,17 @@ function codetot_wc_remove_product_schema_product_archive()
   remove_action('woocommerce_shop_loop', array(WC()->structured_data, 'generate_product_data'), 10, 0);
 }
 add_action('woocommerce_init', 'codetot_wc_remove_product_schema_product_archive');
+
+/**
+ * Use SKU as gtin8 in structured data
+ */
+function codetot_wc_add_gtin8($markup, $product)
+{
+  if (empty($markup['sku'])) {
+    $markup['sku'] = 'N/A';
+  }
+
+  $markup['gtin8'] = str_replace('-', '', $markup['sku']);
+  return $markup;
+}
+add_filter('woocommerce_structured_data_product', 'codetot_wc_add_gtin8', 10, 2);
