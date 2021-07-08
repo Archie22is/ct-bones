1cc59bdbc3eae660b458a37428f58466da879135
Khoi Nguyen <khoi@codetot.com>
2021-07-08 13:59:42 +0700

    Fix coding standard

--- codetot/seo-support.php
+++ codetot/seo-support.php
@@ -6,7 +6,8 @@
  * @param [type] $entity
  * @return void
  */
-function codetot_rankmath_rich_snippet_product_entity($entity) {
+function codetot_rankmath_rich_snippet_product_entity($entity)
+{
   if (empty($entity['brand'])) {
     $entity['brand'] = get_bloginfo('name');
   }
@@ -17,12 +18,13 @@ function codetot_rankmath_rich_snippet_product_entity($entity) {
 
   return $entity;
 }
-add_filter( 'rank_math/snippet/rich_snippet_product_entity', 'codetot_rankmath_rich_snippet_product_entity');
+add_filter('rank_math/snippet/rich_snippet_product_entity', 'codetot_rankmath_rich_snippet_product_entity');
 
 /**
  * Remove the generated product schema markup from Product Category and Shop pages.
  */
-function codetot_wc_remove_product_schema_product_archive() {
-	remove_action( 'woocommerce_shop_loop', array( WC()->structured_data, 'generate_product_data' ), 10, 0 );
+function codetot_wc_remove_product_schema_product_archive()
+{
+  remove_action('woocommerce_shop_loop', array(WC()->structured_data, 'generate_product_data'), 10, 0);
 }
-add_action( 'woocommerce_init', 'codetot_wc_remove_product_schema_product_archive' );
+add_action('woocommerce_init', 'codetot_wc_remove_product_schema_product_archive');
