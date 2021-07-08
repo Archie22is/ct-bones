fcc2e2282e12e627611211e42eb4aec59a9587db
Khoi Nguyen <khoi@codetot.com>
2021-07-08 13:51:55 +0700

    Disable product schema in product archive page

--- codetot/seo-support.php
+++ codetot/seo-support.php
@@ -1,7 +1,5 @@
 <?php
 
-add_filter( 'rank_math/snippet/rich_snippet_product_entity', 'codetot_rankmath_rich_snippet_product_entity');
-
 /**
  * Fix case Either “offers”, “review”, or “aggregateRating” should be specified
  *
@@ -19,3 +17,12 @@ function codetot_rankmath_rich_snippet_product_entity($entity) {
 
   return $entity;
 }
+add_filter( 'rank_math/snippet/rich_snippet_product_entity', 'codetot_rankmath_rich_snippet_product_entity');
+
+/**
+ * Remove the generated product schema markup from Product Category and Shop pages.
+ */
+function codetot_wc_remove_product_schema_product_archive() {
+	remove_action( 'woocommerce_shop_loop', array( WC()->structured_data, 'generate_product_data' ), 10, 0 );
+}
+add_action( 'woocommerce_init', 'codetot_wc_remove_product_schema_product_archive' );
