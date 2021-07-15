3c0506f9a6abb194b3979a2d347426d0f6d82f8f
Khoi Nguyen <khoi@codetot.com>
2021-07-15 20:47:35 +0700

    Fix passing 'lazyload' false to block image

--- blocks/image.php
+++ blocks/image.php
@@ -1,5 +1,5 @@
 <?php
-$_no_lazyload = isset($lazyload) && $lazyload ? $lazyload : false;
+$_lazyload = isset($lazyload) && $lazyload ?? true;
 $_size = !empty($size) ? $size : 'full';
 
 if (!empty($image) && !empty($class)) :
@@ -8,22 +8,31 @@ if (!empty($image) && !empty($class)) :
 
   if (!empty($image['ID'])) {
     $image_id = $image['ID'];
-  } elseif (is_int( (int) $image_id)) {
+  } elseif (is_int( (int) $image) ) {
     $image_id = $image;
   } else {
     echo '<-- Undefined image -->';
+
   }
   ?>
   <picture class="image <?php echo $class; ?>">
     <?php
-    $image_html = wp_get_attachment_image($image_id, $_size, false, array(
-      'loading' => false,
-      'class' => 'wp-post-image image__img lazyload',
-    ));
 
-    $image_html = str_replace('srcset="', 'srcset="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-srcset="', $image_html);
-    $image_html = preg_replace('/[ ]sizes="[^"]*"/', '', $image_html);
-    $image_html = str_replace(' data-srcset="', ' data-sizes="auto" data-srcset="', $image_html);
+    if ($_lazyload) :
+      $image_html = wp_get_attachment_image($image_id, $_size, false, array(
+        'loading' => false,
+        'class' => 'wp-post-image image__img lazyload',
+      ));
+
+      $image_html = str_replace('srcset="', 'srcset="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-srcset="', $image_html);
+      $image_html = preg_replace('/[ ]sizes="[^"]*"/', '', $image_html);
+      $image_html = str_replace(' data-srcset="', ' data-sizes="auto" data-srcset="', $image_html);
+    else :
+      $image_html = wp_get_attachment_image($image_id, $_size, false, array(
+        'loading' => false,
+        'class' => 'wp-post-image image__img',
+      ));
+    endif;
 
     echo $image_html;
     ?>
