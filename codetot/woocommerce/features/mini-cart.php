12a9397cb9c36a9845736c1bd5288ffa8d255e0d
Khoi Nguyen <khoi@codetot.com>
2021-08-23 18:33:48 +0700

    Integrate customize settings mini cart feature

--- codetot/woocommerce/features/mini-cart.php
+++ codetot/woocommerce/features/mini-cart.php
@@ -33,7 +33,7 @@ class Codetot_Woocommerce_Mini_Cart extends Codetot_Woocommerce_Layout
 
   private function __construct()
   {
-    $this->enable = !empty(get_global_option('codetot_woocommerce_enable_minicart'));
+    $this->enable = codetot_get_theme_mod('enable_mini_cart', 'woocommerce') ?? true;
 
     if ($this->enable) {
       add_action('wp_footer', array($this, 'woocommerce_cart_sidebar'));
