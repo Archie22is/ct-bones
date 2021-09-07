eb1ce500728c25dd5f17ea5ab9b1638155eb19f1
Khoi Nguyen <khoi@codetot.com>
2021-09-07 16:20:50 +0700

    Format php files in features/

--- codetot/features/back-to-top.php
+++ codetot/features/back-to-top.php
@@ -4,41 +4,42 @@ if (!defined('ABSPATH')) exit;
 
 class Codetot_Back_To_Top
 {
-  /**
-   * Singleton instance
-   *
-   * @var Codetot_Back_To_Top
-   */
-  private static $instance;
+	/**
+	 * Singleton instance
+	 *
+	 * @var Codetot_Back_To_Top
+	 */
+	private static $instance;
 
-  /**
-   * Get singleton instance.
-   *
-   * @return Codetot_Back_To_Top
-   */
-  public final static function instance()
-  {
-    if (is_null(self::$instance)) {
-      self::$instance = new self();
-    }
-    return self::$instance;
-  }
+	/**
+	 * Get singleton instance.
+	 *
+	 * @return Codetot_Back_To_Top
+	 */
+	public final static function instance()
+	{
+		if (is_null(self::$instance)) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
 
-    /**
-     * Class constructor
-     */
-  public function __construct()
-  {
-    $enable = codetot_get_theme_mod('enable_back_to_top', 'pro') ?? true;
+	/**
+	 * Class constructor
+	 */
+	public function __construct()
+	{
+		$enable = codetot_get_theme_mod('enable_back_to_top', 'pro') ?? true;
 
-    if ($enable) {
-      add_action('codetot_footer', 'codetot_render_back_to_top_section', 30);
-    }
-  }
+		if ($enable) {
+			add_action('codetot_footer', 'codetot_render_back_to_top_section', 30);
+		}
+	}
 }
 
-function codetot_render_back_to_top_section() {
-  the_block('back-to-top');
+function codetot_render_back_to_top_section()
+{
+	the_block('back-to-top');
 }
 
 Codetot_Back_To_Top::instance();
--- codetot/features/facebook-comments.php
+++ codetot/features/facebook-comments.php
@@ -4,104 +4,108 @@ if (!defined('ABSPATH')) exit;
 
 class Codetot_Facebook_Comments
 {
-  /**
-   * Singleton instance
-   *
-   * @var Codetot_Facebook_Comments
-   */
-  private static $instance;
+	/**
+	 * Singleton instance
+	 *
+	 * @var Codetot_Facebook_Comments
+	 */
+	private static $instance;
 
-  /**
-   * Get singleton instance.
-   *
-   * @return Codetot_Facebook_Comments
-   */
-  public final static function instance()
-  {
-    if (is_null(self::$instance)) {
-      self::$instance = new self();
-    }
-    return self::$instance;
-  }
+	/**
+	 * Get singleton instance.
+	 *
+	 * @return Codetot_Facebook_Comments
+	 */
+	public final static function instance()
+	{
+		if (is_null(self::$instance)) {
+			self::$instance = new self();
+		}
+		return self::$instance;
+	}
 
-    /**
-     * Class constructor
-     */
-  public function __construct()
-  {
-    $enable_post_comment = codetot_get_theme_mod('single_post_enable_facebook_comment') ?? false;
-    if ($enable_post_comment) {
-      add_action('codetot_after_post', array($this, 'load_facebook_comments'), 20);
-    }
+	/**
+	 * Class constructor
+	 */
+	public function __construct()
+	{
+		$enable_post_comment = codetot_get_theme_mod('single_post_enable_facebook_comment') ?? false;
+		if ($enable_post_comment) {
+			add_action('codetot_after_post', array($this, 'load_facebook_comments'), 20);
+		}
 
-    $enable_product_comment = (class_exists('WooCommerce') && codetot_get_theme_mod('single_product_enable_facebook_comment')) ?? false;
-    if ($enable_product_comment) {
-      // Add new tab
-      add_filter( 'woocommerce_product_tabs', array($this, 'product_tabs_facebook_reviews'), 98 );
-    }
-  }
+		$enable_product_comment = (class_exists('WooCommerce') && codetot_get_theme_mod('single_product_enable_facebook_comment')) ?? false;
+		if ($enable_product_comment) {
+			// Add new tab
+			add_filter('woocommerce_product_tabs', array($this, 'product_tabs_facebook_reviews'), 98);
+		}
+	}
 
-  public function product_tabs_facebook_reviews($tabs) {
-    if (
-      get_option( 'woocommerce_enable_reviews') === 'yes' &&
-      !empty($tabs['reviews'])
-    ) {
-      unset($tabs['reviews']);
-    }
+	public function product_tabs_facebook_reviews($tabs)
+	{
+		if (
+			get_option('woocommerce_enable_reviews') === 'yes' &&
+			!empty($tabs['reviews'])
+		) {
+			unset($tabs['reviews']);
+		}
 
-    if (!empty($tabs)) :
-      $tabs['reviews'] = array(
-        'title' => apply_filters('woocommerce_reviews_title', esc_html__('Reviews', 'woocommerce')),
-        'priority' => 90,
-        'callback' => array($this, 'load_facebook_comments')
-      );
+		if (!empty($tabs)) :
+			$tabs['reviews'] = array(
+				'title' => apply_filters('woocommerce_reviews_title', esc_html__('Reviews', 'woocommerce')),
+				'priority' => 90,
+				'callback' => array($this, 'load_facebook_comments')
+			);
 
-    endif;
+		endif;
 
-    return $tabs;
-  }
+		return $tabs;
+	}
 
-  /**
-   * Load Javascript SDK
-   *
-   * Taken from https://developers.facebook.com/docs/plugins/comments
-   *
-   * @return void
-   */
-  public function load_script() {
-    ?>
-    <div id="fb-root"></div>
-    <script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v10.0" nonce="7Q8qKRn1"></script>
-    <?php
-  }
+	/**
+	 * Load Javascript SDK
+	 *
+	 * Taken from https://developers.facebook.com/docs/plugins/comments
+	 *
+	 * @return void
+	 */
+	public function load_script()
+	{
+?>
+		<div id="fb-root"></div>
+		<script async defer crossorigin="anonymous" src="https://connect.facebook.net/vi_VN/sdk.js#xfbml=1&version=v10.0" nonce="7Q8qKRn1"></script>
+	<?php
+	}
 
-  public function load_embed($url) {
-    ?>
-    <div class="fb-comments" data-href="<?php echo esc_url($url); ?>" data-width="100%" data-numposts="5"></div>
-    <?php
-  }
+	public function load_embed($url)
+	{
+	?>
+		<div class="fb-comments" data-href="<?php echo esc_url($url); ?>" data-width="100%" data-numposts="5"></div>
+<?php
+	}
 
-  public function load_facebook_comments() {
-    if (!is_singular()) {
-      return;
-    }
+	public function load_facebook_comments()
+	{
+		if (!is_singular()) {
+			return;
+		}
 
-    if ($this->is_localhost()) {
-      echo '<--- WARNING: Facebook comments can\'t load in localhost environment. You must test on live or staging environment. -->';
-    } else {
-      global $post;
+		if ($this->is_localhost()) {
+			echo '<--- WARNING: Facebook comments can\'t load in localhost environment. You must test on live or staging environment. -->';
+		} else {
+			global $post;
 
-      $url = get_permalink($post);
+			$url = get_permalink($post);
 
-      $this->load_script();
-      $this->load_embed($url);
-    }
-  }
+			$this->load_script();
+			$this->load_embed($url);
+		}
+	}
 
-  public function is_localhost()
-  {
-    return !empty($_SERVER['HTTP_X_CODETOT_HEADER']) && $_SERVER['HTTP_X_CODETOT_HEADER'] === 'development';
-  }
+	public function is_localhost()
+	{
+		return !empty($_SERVER['HTTP_X_CODETOT_HEADER']) && $_SERVER['HTTP_X_CODETOT_HEADER'] === 'development';
+	}
 }
 
 Codetot_Facebook_Comments::instance();
