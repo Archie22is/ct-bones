3335415eb0b2faa2c37df37d77536094aa56d426
Khoi Nguyen <khoi@codetot.com>
2021-09-07 16:22:55 +0700

    Format file in widgets/

--- codetot/widgets/ct-icon-box.php
+++ codetot/widgets/ct-icon-box.php
@@ -1,209 +1,215 @@
 <?php
 
-class codetot_widget_icon extends WP_Widget {
-  function __construct() {
-    parent::__construct(
-      'codetot_widget_icon',
-      sprintf(__('%s Icon Box', 'ct-bones'), '[CT]'),
-      array(
-        'description' => esc_html__('Display the guarantee list widget.', 'ct-bones')
-      )
-    );
-  }
+class codetot_widget_icon extends WP_Widget
+{
+	function __construct()
+	{
+		parent::__construct(
+			'codetot_widget_icon',
+			sprintf(__('%s Icon Box', 'ct-bones'), '[CT]'),
+			array(
+				'description' => esc_html__('Display the guarantee list widget.', 'ct-bones')
+			)
+		);
+	}
 
-  function form( $instance ) {
-    $default = array();
-    $instance = wp_parse_args( (array) $instance, $default );
-  }
+	function form($instance)
+	{
+		$default = array();
+		$instance = wp_parse_args((array) $instance, $default);
+	}
 
-  function update( $new_instance, $old_instance ) {
-    $instance = $old_instance;
-    return $instance;
-  }
+	function update($new_instance, $old_instance)
+	{
+		$instance = $old_instance;
+		return $instance;
+	}
 
-  function widget( $args, $instance ) {
-    extract($args);
+	function widget($args, $instance)
+	{
+		extract($args);
 
-    $title = get_field('title','widget_' . $args["widget_id"]);
-    $items = get_field('items','widget_' . $args["widget_id"]);
-    $layout = get_field('layout','widget_' . $args["widget_id"]);
+		$title = get_field('title', 'widget_' . $args["widget_id"]);
+		$items = get_field('items', 'widget_' . $args["widget_id"]);
+		$layout = get_field('layout', 'widget_' . $args["widget_id"]);
 
-    echo '<div class="widget widget--custom widget--ct_icon_box">';
-    if (!empty($title)) :
-      printf('<p class="widget__title">%s</p>', $title);
-    endif;
-    if(!empty($items)) {
-      echo '<div class="widget--ct_icon_box_main">';
-      foreach ($items as $item) {
-        the_block('ct-widget-icon',array(
-          'style' => $layout,
-          'icon' => $item['icon_image'],
-          'title' => $item['title'],
-          'description' => $item['description']
-        ));
-      }
-      echo '</div>';
-    }
-    echo '</div>';
-  }
+		echo '<div class="widget widget--custom widget--ct_icon_box">';
+		if (!empty($title)) :
+			printf('<p class="widget__title">%s</p>', $title);
+		endif;
+		if (!empty($items)) {
+			echo '<div class="widget--ct_icon_box_main">';
+			foreach ($items as $item) {
+				the_block('ct-widget-icon', array(
+					'style' => $layout,
+					'icon' => $item['icon_image'],
+					'title' => $item['title'],
+					'description' => $item['description']
+				));
+			}
+			echo '</div>';
+		}
+		echo '</div>';
+	}
 }
 
-add_action( 'widgets_init', 'create_codetot_widget_icon' );
-function create_codetot_widget_icon() {
-  register_widget('codetot_widget_icon');
+add_action('widgets_init', 'create_codetot_widget_icon');
+function create_codetot_widget_icon()
+{
+	register_widget('codetot_widget_icon');
 }
 
 
-if( function_exists('acf_add_local_field_group') ):
-  acf_add_local_field_group(array(
-    'key' => 'group_60110eae81354',
-    'title' => '[CT] Icon Box',
-    'fields' => array(
-      array(
-        'key' => 'field_60110f0734a5e',
-        'label' => 'Title',
-        'name' => 'title',
-        'type' => 'text',
-        'instructions' => '',
-        'required' => 0,
-        'conditional_logic' => 0,
-        'wrapper' => array(
-          'width' => '',
-          'class' => '',
-          'id' => '',
-        ),
-        'default_value' => '',
-        'placeholder' => '',
-        'prepend' => '',
-        'append' => '',
-        'maxlength' => '',
-      ),
-      array(
-        'key' => 'field_60823801d34b6',
-        'label' => 'Main Layout',
-        'name' => 'layout',
-        'type' => 'select',
-        'instructions' => '',
-        'required' => 0,
-        'conditional_logic' => 0,
-        'wrapper' => array(
-          'width' => '',
-          'class' => '',
-          'id' => '',
-        ),
-        'choices' => array(
-          'left' => 'Left',
-          'center' => 'Center',
-          'right' => 'Right',
-        ),
-        'default_value' => 'left',
-        'allow_null' => 0,
-        'multiple' => 0,
-        'ui' => 0,
-        'return_format' => 'value',
-        'ajax' => 0,
-        'placeholder' => '',
-      ),
-      array(
-        'key' => 'field_60110f1034a5f',
-        'label' => 'Items',
-        'name' => 'items',
-        'type' => 'repeater',
-        'instructions' => '',
-        'required' => 0,
-        'conditional_logic' => 0,
-        'wrapper' => array(
-          'width' => '',
-          'class' => '',
-          'id' => '',
-        ),
-        'collapsed' => '',
-        'min' => 0,
-        'max' => 0,
-        'layout' => 'block',
-        'button_label' => esc_html__('Add Item', 'ct-bones'),
-        'sub_fields' => array(
-          array(
-            'key' => 'field_60110fc034a63',
-            'label' => 'Icon',
-            'name' => 'icon_image',
-            'type' => 'image',
-            'instructions' => '',
-            'required' => 0,
-            'conditional_logic' => 0,
-            'wrapper' => array(
-              'width' => '',
-              'class' => '',
-              'id' => '',
-            ),
-            'return_format' => 'array',
-            'preview_size' => 'medium',
-            'library' => 'all',
-            'min_width' => '',
-            'min_height' => '',
-            'min_size' => '',
-            'max_width' => '',
-            'max_height' => '',
-            'max_size' => '',
-            'mime_types' => '',
-          ),
-          array(
-            'key' => 'field_60110f1c34a60',
-            'label' => 'Title',
-            'name' => 'title',
-            'type' => 'text',
-            'instructions' => '',
-            'required' => 0,
-            'conditional_logic' => 0,
-            'wrapper' => array(
-              'width' => '',
-              'class' => '',
-              'id' => '',
-            ),
-            'default_value' => '',
-            'placeholder' => '',
-            'prepend' => '',
-            'append' => '',
-            'maxlength' => '',
-          ),
-          array(
-            'key' => 'field_60110dsa34a60',
-            'label' => 'Description',
-            'name' => 'description',
-            'type' => 'text',
-            'instructions' => '',
-            'required' => 0,
-            'conditional_logic' => 0,
-            'wrapper' => array(
-              'width' => '',
-              'class' => '',
-              'id' => '',
-            ),
-            'default_value' => '',
-            'placeholder' => '',
-            'prepend' => '',
-            'append' => '',
-            'maxlength' => '',
-          ),
-        ),
-      ),
-    ),
-    'location' => array(
-      array(
-        array(
-          'param' => 'widget',
-          'operator' => '==',
-          'value' => 'codetot_widget_icon',
-        ),
-      ),
-    ),
-    'menu_order' => 0,
-    'position' => 'normal',
-    'style' => 'default',
-    'label_placement' => 'top',
-    'instruction_placement' => 'label',
-    'hide_on_screen' => '',
-    'active' => true,
-    'description' => '',
-  ));
+if (function_exists('acf_add_local_field_group')) :
+	acf_add_local_field_group(array(
+		'key' => 'group_60110eae81354',
+		'title' => '[CT] Icon Box',
+		'fields' => array(
+			array(
+				'key' => 'field_60110f0734a5e',
+				'label' => 'Title',
+				'name' => 'title',
+				'type' => 'text',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'default_value' => '',
+				'placeholder' => '',
+				'prepend' => '',
+				'append' => '',
+				'maxlength' => '',
+			),
+			array(
+				'key' => 'field_60823801d34b6',
+				'label' => 'Main Layout',
+				'name' => 'layout',
+				'type' => 'select',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'choices' => array(
+					'left' => 'Left',
+					'center' => 'Center',
+					'right' => 'Right',
+				),
+				'default_value' => 'left',
+				'allow_null' => 0,
+				'multiple' => 0,
+				'ui' => 0,
+				'return_format' => 'value',
+				'ajax' => 0,
+				'placeholder' => '',
+			),
+			array(
+				'key' => 'field_60110f1034a5f',
+				'label' => 'Items',
+				'name' => 'items',
+				'type' => 'repeater',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'collapsed' => '',
+				'min' => 0,
+				'max' => 0,
+				'layout' => 'block',
+				'button_label' => esc_html__('Add Item', 'ct-bones'),
+				'sub_fields' => array(
+					array(
+						'key' => 'field_60110fc034a63',
+						'label' => 'Icon',
+						'name' => 'icon_image',
+						'type' => 'image',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'return_format' => 'array',
+						'preview_size' => 'medium',
+						'library' => 'all',
+						'min_width' => '',
+						'min_height' => '',
+						'min_size' => '',
+						'max_width' => '',
+						'max_height' => '',
+						'max_size' => '',
+						'mime_types' => '',
+					),
+					array(
+						'key' => 'field_60110f1c34a60',
+						'label' => 'Title',
+						'name' => 'title',
+						'type' => 'text',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'default_value' => '',
+						'placeholder' => '',
+						'prepend' => '',
+						'append' => '',
+						'maxlength' => '',
+					),
+					array(
+						'key' => 'field_60110dsa34a60',
+						'label' => 'Description',
+						'name' => 'description',
+						'type' => 'text',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'default_value' => '',
+						'placeholder' => '',
+						'prepend' => '',
+						'append' => '',
+						'maxlength' => '',
+					),
+				),
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'widget',
+					'operator' => '==',
+					'value' => 'codetot_widget_icon',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => true,
+		'description' => '',
+	));
 endif;
--- codetot/widgets/ct-related-posts.php
+++ codetot/widgets/ct-related-posts.php
@@ -1,9 +1,10 @@
 <?php
-add_action( 'widgets_init', 'related_posts_widget' );
+add_action('widgets_init', 'related_posts_widget');
 
-function related_posts_widget() {
+function related_posts_widget()
+{
 
-	register_widget( 'Codetot_Related_Post_Widget' );
+	register_widget('Codetot_Related_Post_Widget');
 }
 
 /**
@@ -11,30 +12,33 @@ function related_posts_widget() {
  *
  * @since 2.8.0
  */
-class Codetot_Related_Post_Widget extends WP_Widget {
+class Codetot_Related_Post_Widget extends WP_Widget
+{
 
-	function __construct() {
-    parent::__construct(
-      'codetot_related_posts',
-      sprintf(__('%s  Related Posts', 'ct-bones'), '[CT]'),
-      array(
-        'description' => esc_html__('A widget that displays related posts.', 'ct-bones')
-      )
-    );
+	function __construct()
+	{
+		parent::__construct(
+			'codetot_related_posts',
+			sprintf(__('%s  Related Posts', 'ct-bones'), '[CT]'),
+			array(
+				'description' => esc_html__('A widget that displays related posts.', 'ct-bones')
+			)
+		);
 	}
 
-	function widget($args, $instance) {
+	function widget($args, $instance)
+	{
 
 		$cache = wp_cache_get('widget_related_posts', 'widget');
 
-		if ( !is_array($cache) )
+		if (!is_array($cache))
 			$cache = array();
 
-		if ( !isset( $args['widget_id'] ) )
+		if (!isset($args['widget_id']))
 			$args['widget_id'] = $this->id;
 
-		if ( isset( $cache[ $args['widget_id'] ] ) ) {
-			echo $cache[ $args['widget_id'] ];
+		if (isset($cache[$args['widget_id']])) {
+			echo $cache[$args['widget_id']];
 			return;
 		}
 
@@ -42,49 +46,49 @@ class Codetot_Related_Post_Widget extends WP_Widget {
 		extract($args);
 
 		$title = apply_filters('widget_title', empty($instance['title']) ? __('Related Posts', 'codetot') : $instance['title'], $instance, $this->id_base);
-		if ( empty( $instance['number'] ) || ! $number = absint( $instance['number'] ) )
- 			$number = 10;
+		if (empty($instance['number']) || !$number = absint($instance['number']))
+			$number = 10;
 
-    global $post;
+		global $post;
 
-    $args_resent_post = array(
-        'post_status' => 'publish',
-        'post_type' => 'post',
-        'posts_per_page' => $number,
-      );
-    if(is_single()) {
-      $args_resent_post['category__in'] = wp_get_post_categories( $post->ID );
-      $args_resent_post['post__not_in'] = array( $post->ID );
-    }
+		$args_resent_post = array(
+			'post_status' => 'publish',
+			'post_type' => 'post',
+			'posts_per_page' => $number,
+		);
+		if (is_single()) {
+			$args_resent_post['category__in'] = wp_get_post_categories($post->ID);
+			$args_resent_post['post__not_in'] = array($post->ID);
+		}
 		$query = new WP_Query($args_resent_post);
 		if ($query->have_posts()) :
-    ?>
-		<?php echo $before_widget; ?>
-		<?php if ( $title ) echo $before_title . $title . $after_title; ?>
-		<div class="widget__list">
-      <?php while ( $query->have_posts() ) : $query->the_post(); ?>
-        <div class="w100 widget__item">
-          <span class="f widget__item-wrapper">
-            <a class="f widget__image-link" href="<?php the_permalink() ?>">
-              <?php if (has_post_thumbnail()) :
-                echo get_the_post_thumbnail(get_the_id(), 'thumbnail');
-                else :
-                  the_block('image-placeholder');
-                endif;
-              ?>
-            </a>
-            <span class="f fdc widget__content">
-              <span class="f fw widget__item-title"><a class="d-block w100 bold-text widget__item-title-link" href="<?php the_permalink() ?>"><?php the_title(); ?></a></span>
-              <span class="d-block mt-05 post-date widget__post-date"><?php echo get_the_date('d/m/Y', get_the_ID()); ?></span>
-            </span>
-          </span>
-        </div>
-      <?php endwhile; ?>
-		</div>
-		<?php echo $after_widget; ?>
-  <?php
-		// Reset the global $the_post as this query will have stomped on it
-		wp_reset_postdata();
+?>
+			<?php echo $before_widget; ?>
+			<?php if ($title) echo $before_title . $title . $after_title; ?>
+			<div class="widget__list">
+				<?php while ($query->have_posts()) : $query->the_post(); ?>
+					<div class="w100 widget__item">
+						<span class="f widget__item-wrapper">
+							<a class="f widget__image-link" href="<?php the_permalink() ?>">
+								<?php if (has_post_thumbnail()) :
+									echo get_the_post_thumbnail(get_the_id(), 'thumbnail');
+								else :
+									the_block('image-placeholder');
+								endif;
+								?>
+							</a>
+							<span class="f fdc widget__content">
+								<span class="f fw widget__item-title"><a class="d-block w100 bold-text widget__item-title-link" href="<?php the_permalink() ?>"><?php the_title(); ?></a></span>
+								<span class="d-block mt-05 post-date widget__post-date"><?php echo get_the_date('d/m/Y', get_the_ID()); ?></span>
+							</span>
+						</span>
+					</div>
+				<?php endwhile; ?>
+			</div>
+			<?php echo $after_widget; ?>
+		<?php
+			// Reset the global $the_post as this query will have stomped on it
+			wp_reset_postdata();
 
 		endif;
 
@@ -92,33 +96,37 @@ class Codetot_Related_Post_Widget extends WP_Widget {
 		wp_cache_set('widget_related_posts', $cache, 'widget');
 	}
 
-	function update( $new_instance, $old_instance ) {
+	function update($new_instance, $old_instance)
+	{
 		$instance = $old_instance;
 		$instance['title'] = strip_tags($new_instance['title']);
 		$instance['number'] = (int) $new_instance['number'];
 		$this->flush_widget_cache();
 
-		$alloptions = wp_cache_get( 'alloptions', 'options' );
-		if ( isset($alloptions['widget_related_entries']) )
+		$alloptions = wp_cache_get('alloptions', 'options');
+		if (isset($alloptions['widget_related_entries']))
 			delete_option('widget_related_entries');
 
 		return $instance;
 	}
 
-	function flush_widget_cache() {
+	function flush_widget_cache()
+	{
 		wp_cache_delete('widget_related_posts', 'widget');
 	}
 
-	function form( $instance ) {
-		$title     = isset( $instance['title'] ) ? esc_attr( $instance['title'] ) : '';
-		$number    = isset( $instance['number'] ) ? absint( $instance['number'] ) : 5;
-?>
-		<p><label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:', 'ct-bones' ); ?></label>
-		<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" /></p>
+	function form($instance)
+	{
+		$title     = isset($instance['title']) ? esc_attr($instance['title']) : '';
+		$number    = isset($instance['number']) ? absint($instance['number']) : 5;
+		?>
+		<p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'ct-bones'); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo $title; ?>" />
+		</p>
 
-		<p><label for="<?php echo $this->get_field_id( 'number' ); ?>"><?php _e( 'Number of posts to show:', 'ct-bones' ); ?></label>
-		<input id="<?php echo $this->get_field_id( 'number' ); ?>" name="<?php echo $this->get_field_name( 'number' ); ?>" type="text" value="<?php echo $number; ?>" size="3" /></p>
+		<p><label for="<?php echo $this->get_field_id('number'); ?>"><?php _e('Number of posts to show:', 'ct-bones'); ?></label>
+			<input id="<?php echo $this->get_field_id('number'); ?>" name="<?php echo $this->get_field_name('number'); ?>" type="text" value="<?php echo $number; ?>" size="3" />
+		</p>
 <?php
 	}
 }
-?>
