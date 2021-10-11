0f949d7251c4354a46498adeb6359136136f6bee
CODE TOT JSC <dev@codetot.com>
2021-10-11 15:14:26 +0700

    Refactor single post layout to ensure it could be override by single.php in child theme

--- template-parts/content.php
+++ template-parts/content.php
@@ -7,11 +7,13 @@
  *
  * @package CT_Bones
  */
+global $post;
 
 $hide_featured_image = codetot_get_theme_mod('hide_featured_image') ?? false;
 $hide_post_meta      = codetot_get_theme_mod('hide_post_meta') ?? false;
 $_hide_post_meta = 'post' === get_post_type() && !$hide_post_meta;
-$hide_header = apply_filters('codetot_hide_single_post_header', false);
+$enable_hero_image = codetot_get_theme_mod('extra_single_post_layout', 'pro') ?? 'none';
+$hide_header = apply_filters('codetot_hide_single_post_header', $enable_hero_image === 'hero_image');
 
 ob_start();
 ct_bones_posted_on();
@@ -30,6 +32,7 @@ else :
   the_title('<h2 class="entry-title"><a href="' . esc_url(get_permalink()) . '" rel="bookmark">', '</a></h2>');
 endif;
 $header = ob_get_clean();
+
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
@@ -44,7 +47,7 @@ $header = ob_get_clean();
       <?php endif;
       ?>
     </header>
-  <?php endif; ?>
+	<?php endif; ?>
   <?php if (!$hide_featured_image) : ?>
     <div class="entry-thumbnail">
       <?php ct_bones_post_thumbnail(); ?>
