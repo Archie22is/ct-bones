89125f781c0904880a7d2b9a8bb74f855fc3830c
CODE TOT JSC <dev@codetot.com>
2021-11-20 15:14:01 +0700

    Fix page header render

--- blocks/page-header.php
+++ blocks/page-header.php
@@ -1,15 +1,17 @@
 <?php
-$container = 'container';
-$_class    = 'page-header';
+$_class    = 'section wp-block-group page-header';
 $_class   .= ! empty( $alignment ) ? ' ' . esc_attr( $alignment ) : '';
 $_class   .= ! empty( $class ) ? ' ' . esc_attr( $class ) : '';
+
 do_action( 'codetot_page_header_before' ); ?>
+
 <div class="<?php echo $_class; ?>">
-  <div class="<?php echo $container; ?> page-header__container">
-	<h1 class="m0 page-header__title"><?php echo $title; ?></h1>
+  <div class="wp-block-group__inner-container">
+	<h1 class="m0 has-heading-2-font-size page-header__title"><?php echo $title; ?></h1>
 	<?php if ( ! empty( $description ) ) : ?>
 	  <div class="page-header__description"><?php echo $description; ?></div>
 	<?php endif; ?>
   </div>
 </div>
+
 <?php do_action( 'codetot_page_header_after' ); ?>
