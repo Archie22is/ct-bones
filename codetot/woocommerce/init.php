71c4c3e42eb4dfd341884249d44201f2a1be3a6d
CODE TOT JSC <dev@codetot.com>
2021-11-24 15:23:56 +0700

    Fix  filter with woocommercer block

--- codetot/woocommerce/init.php
+++ codetot/woocommerce/init.php
@@ -34,7 +34,7 @@ class Codetot_WooCommerce_Init {
 		add_action( 'after_setup_theme', array( $this, 'woocommerce_support' ) );
 		add_action( 'widgets_init', array( $this, 'register_woocommerce_sidebars' ) );
 
-		add_filter( 'woocommerce_product_add_to_cart_text', array( $this, 'update_add_to_cart_button_text' ) );
+		add_filter( 'woocommerce_product_add_to_cart_text', array( $this, 'update_add_to_cart_button_text' ), 10, 2 );
 
 		add_filter( 'woocommerce_breadcrumb_defaults', array( $this, 'breadcrumbs_container' ) );
 		add_action( 'pre_get_posts', array( $this, 'search_product_only' ) );
@@ -157,9 +157,7 @@ class Codetot_WooCommerce_Init {
 	  endif;
 	}
 
-	public function update_add_to_cart_button_text( $text ) {
-		global $product;
-
+	public function update_add_to_cart_button_text( $text, $product ) {
 		if ( $product->is_type( 'variable' ) ) {
 			$variations = $product->get_available_variations();
 
@@ -185,7 +183,7 @@ class Codetot_WooCommerce_Init {
 		  }
 		})(jQuery);
 	  </script>
-			<?php 
+			<?php
 endif;
 	}
 
