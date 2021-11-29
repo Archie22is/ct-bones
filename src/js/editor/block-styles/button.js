813c72229757f8ade1b533a6a78855eff34d7155
CODE TOT JSC <dev@codetot.com>
2021-11-29 03:52:14 +0000

    Prepare production package

--- src/js/editor/block-styles/button.js
+++
@@ -1,9 +0,0 @@
-/* global wp */
-import { __ } from '@wordpress/i18n'
-
-export default () => {
-	wp.blocks.registerBlockStyle('core/button', {
-		name: 'link',
-		label: __('Link', 'ct-bones')
-	})
-}
