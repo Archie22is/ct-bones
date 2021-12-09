c8d184a6c9df5064dd84177283aee627baf9581f
CODE TOT JSC <dev@codetot.com>
2021-12-09 03:09:49 +0000

    Prepare production package

--- src/js/woocommerce-blocks/modal-login.js
+++
@@ -1,9 +0,0 @@
-import Modal from 'lib/modal'
-
-export default el => {
-	// eslint-disable-next-line no-unused-vars
-	const instance = Modal(el, {
-		id: 'modal-login',
-		closeTriggers: ['.js-close-account-modal']
-	})
-}
