510679a60a597cfb63526cf803837bf32fca4c78
Khoi Nguyen <khoi@codetot.com>
2022-06-23 05:56:42 +0700

    Add custom css class for expand block

--- blocks/expand-block.php
+++ blocks/expand-block.php
@@ -1,5 +1,5 @@
 <?php if ( !empty($content) ) : ?>
-	<div class="expand-block" data-block="expand-block" data-max-height="<?php echo absint($max_height); ?>">
+	<div class="expand-block<?php if ( !empty($class) ) : echo ' ' . $class; endif; ?>" data-block="expand-block" data-max-height="<?php echo absint($max_height); ?>">
 		<div class="expand-block__content js-content" style="position: relative; overflow: hidden; height: <?php echo esc_attr( absint( $max_height) ); ?>px;"><?php echo $content; ?></div>
 		<div class="expand-block__footer">
 			<?php the_block('button', [
